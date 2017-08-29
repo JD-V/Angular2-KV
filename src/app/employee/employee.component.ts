72fc5c1966cd9e2b56ee4c3d9ac4998165bd158d
Jaydeep Vaghasiya <vaghasiyajaydeep@gmail.com>
2017-08-29 11:36:17 +0530

    added externnal stylesheet to component
    using styleUrls

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-employee',
-    templateUrl: 'app/employee/employee.component.html'  //relative to index.html
+    templateUrl: 'app/employee/employee.component.html',  //relative to index.html
+    styleUrls : ['app/employee/employee.component.css'] 
 })
 
 export class EmployeeComponent {
